e830053fa2d3366fbc8853564a859fc98f93131f
Junni2704 <133447019+Junni2704@users.noreply.github.com>
2024-06-08 21:46:44 -0400

    #2 Started working on the layout and Colors
    Added icon, created background, updated theme colour, and the main is   created layout for how transaction list item will look like.

--- BudgetBuddy/app/src/main/java/com/example/budgetbuddy/MainActivity.java
+++ BudgetBuddy/app/src/main/java/com/example/budgetbuddy/MainActivity.java
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import com.example.budgetbuddy.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
 
