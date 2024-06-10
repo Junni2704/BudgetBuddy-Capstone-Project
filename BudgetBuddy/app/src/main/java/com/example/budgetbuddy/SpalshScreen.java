8416f945022ee4256525eefbb910dcb00145cc10
Autsav Adhikari <autsavadhikari@gmail.com>
2024-06-09 20:19:57 -0400

    #1 Implemented Splashscreen
    Closes #1

---
+++ BudgetBuddy/app/src/main/java/com/example/budgetbuddy/SpalshScreen.java
@@ -0,0 +1,27 @@
+package com.example.budgetbuddy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Button btn_splash = findViewById(R.id.btn_splash);
+        btn_splash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+        });
+    }
+}
